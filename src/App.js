2ef5e455ebeb8b34dff878f31d9e9b99e09ab741
Carly Nield <nieldca@byu.edu>
2021-04-21 05:07:17 -0600

    change back

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   // }
 
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="container">
         <a href="/">
           <h1 className="main-header m-3 d-flex">
